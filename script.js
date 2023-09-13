3eb4cc1cc32781d7f5100c973312185cfa93102e
SourabhKanase <141937826+SourabhKanase@users.noreply.github.com>
2023-09-13 18:03:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ let minimumdiff=Infinity;
 let sum=Infinity;	
 for(let i=0;i<arr.length;i++)
 	{
-		for(let j=i+1;j<arr.length-2;j++)
+		for(let j=i+1;j<arr.length;j++)
 			{
-				for(let k=j+1;k<arr.length-1;k++)
+				for(let k=j+1;k<arr.length;k++)
 					{
-					  let x= arr[i]+arr[j]+arr[k]	;
+					  if(i!=j && j!=k && i!=k)
+					  {
+						  let x= arr[i]+arr[j]+arr[k]	;
 						if(Math.abs(x-target)<minimumdiff)
 						{
 							minimumdiff=Math.abs(x-target);
 							sum=x;
 						}
+					  }
 					}
 			}
 	}return sum;
